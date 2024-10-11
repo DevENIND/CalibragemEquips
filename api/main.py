e53be2674985cfba69bfa03d5d9492265f773239
Wagner Araujo Sequeira Barreiro <wagner.barreiro@enind.com.br>
2024-10-11 17:04:27 -0300

    Alterações Vicel

--- api/main.py
+++ api/main.py
@@ -20,10 +20,10 @@ app.register_blueprint(Carr_Img_route, url_prefix="/Carr_Img")
 
 if __name__ == "__main__":
     # Debug/Development
-     app.run(debug=True, host="0.0.0.0", port="5000")
+    #app.run(debug=True, host="0.0.0.0", port="5000")
     # Production
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
     
  
     
