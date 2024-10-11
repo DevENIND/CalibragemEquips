e5dc6da12d2a7cea7bbd4948264ea6f66bac13a4
Wagner Araujo Sequeira Barreiro <wagner.barreiro@enind.com.br>
2024-10-11 10:25:48 -0300

    Alteração Main

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ app.register_blueprint(Carr_Img_route, url_prefix="/Carr_Img")
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT','5000'))
-    app.run(host='0.0.0.0', port = port, debug=True)
+    app.run(host='0.0.0.0', port = port)
  
     
