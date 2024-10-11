1518ed23d28c5e33e8fe0743bb8ff25167b8922f
Wagner Araujo Sequeira Barreiro <wagner.barreiro@enind.com.br>
2024-10-11 14:58:21 -0300

    Publicação Vercer

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from jinja2.utils import F
 from routes.log_eqto import log_eqto_route
 from routes.Ctr_Eqto import Ctr_Eqto_route
 from routes.Carr_Img import Carr_Img_route
+from gevent.pywsgi import WSGIServer
 import os
 
  
@@ -18,6 +19,11 @@ app.register_blueprint(Carr_Img_route, url_prefix="/Carr_Img")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    
  
     
